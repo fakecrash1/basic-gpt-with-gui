7d8392fd93c70be7d22224fc93011423a2a11aae
fakecrash1 <fakecrash@gmail.com>
2023-05-26 06:25:15 +0200

    create the base of the app

--- chatgpt.py
+++ chatgpt.py
@@ -4,3 +4,18 @@ import openai
 import os
 import pickle
 
+# Initiate App
+root = customtkinter.CTk()
+root.title("ChatGPT Bot")
+root.geometry('600x600')
+root.iconbitmap('./icons/ai_lt.ico')
+
+# Set Color Scheme
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+
+
+
+
+root.mainloop()
